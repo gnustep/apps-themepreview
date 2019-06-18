3829d20590662bd9402edebcc9863f4db4f20413
Bertrand Dekoninck <bertrand.dekoninck@gmail.com>
2019-06-18 22:52:09 +0200

    Initial commit

---
+++ AppController.h
@@ -0,0 +1,39 @@
+/* 
+   Project: ThemePreview
+
+   Author: Bertrand,,,
+
+   Created: 2019-06-18 18:17:41 +0200 by bertrand
+   
+   Application Controller
+*/
+ 
+#ifndef _PCAPPPROJ_APPCONTROLLER_H
+#define _PCAPPPROJ_APPCONTROLLER_H
+
+#import <AppKit/AppKit.h>
+// Uncomment if your application is Renaissance-based
+//#import <Renaissance/Renaissance.h>
+
+@interface AppController : NSObject
+{
+}
+
++ (void)  initialize;
+
+- (id) init;
+- (void) dealloc;
+
+- (void) awakeFromNib;
+
+- (void) applicationDidFinishLaunching: (NSNotification *)aNotif;
+- (BOOL) applicationShouldTerminate: (id)sender;
+- (void) applicationWillTerminate: (NSNotification *)aNotif;
+- (BOOL) application: (NSApplication *)application
+	    openFile: (NSString *)fileName;
+
+- (void) showPrefPanel: (id)sender;
+
+@end
+
+#endif
---
+++ ThemePreview_main.m
@@ -0,0 +1,30 @@
+/* 
+   Project: ThemePreview
+
+   Author: Bertrand,,,
+
+   Created: 2019-06-18 18:17:41 +0200 by bertrand
+*/
+
+#import <AppKit/AppKit.h>
+
+int 
+main(int argc, const char *argv[])
+{
+// Uncomment if your application is Renaissance application
+/*  CREATE_AUTORELEASE_POOL (pool);
+  [NSApplication sharedApplication];
+  [NSApp setDelegate: [AppController new]];
+
+  #ifdef GNUSTEP
+    [NSBundle loadGSMarkupNamed: @"MainMenu-GNUstep"  owner: [NSApp delegate]];
+  #else
+    [NSBundle loadGSMarkupNamed: @"MainMenu-OSX"  owner: [NSApp delegate]];
+  #endif
+   
+  RELEASE (pool);
+*/
+
+  return NSApplicationMain (argc, argv);
+}
+
